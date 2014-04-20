45067542914840f5c018891567cf6ca75111632d
Andrei Bucin <abucin@gmail.com>
2014-04-20 13:02:18 +0300

    Optimized installation scripts.

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 npm install -g bower
 npm install -g grunt-cli
 bower install
-npm install grunt --save-dev
-npm install grunt-contrib-uglify --save-dev
-npm install express
-npm install passport
-npm install mongo
+npm install
