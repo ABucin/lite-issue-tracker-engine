7322f1568c1d26174160c338cb0208ce882f9d2d
Andrei Bucin <abucin@gmail.com>
2014-04-22 01:49:39 +0300

    Enabled DB Ticket fetching.

--- server-data.js
+++ server-data.js
@@ -3,36 +3,140 @@ mongoose.connect('mongodb://localhost/issuetracker');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback() {
 
-	var ticketSchema = mongoose.Schema({
-		key: Number,
-		code: String,
-		title: String,
-		status: String,
-		owner: String,
+var persistedTickets = [];
+
+var ticketSchema = mongoose.Schema({
+	code: {
 		type: String,
-		description: String
-	});
+		unique: true,
+		required: true
+	},
+	title: {
+		type: String,
+		required: true
+	},
+	status: {
+		type: String,
+		required: true
+	},
+	owner: {
+		type: String
+	},
+	type: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String
+	}
+});
 
-	var Ticket = mongoose.model("Ticket", ticketSchema);
-
-	var tick = new Ticket({
+var Ticket = mongoose.model("Ticket", ticketSchema);
+/**
+ * Flushes ticket collection and adds some default data.
+ */
+Ticket.collection.drop();
+Ticket.create([
+	{
 		code: "BG-15",
 		title: "Email Validation Not Working",
 		status: "testing",
 		owner: "abucin",
 		type: "bug",
 		description: "The email validation is broken for several users."
-	});
+		}, {
+		code: "BG-21",
+		title: "Authentication Whitespace Handling",
+		status: "created",
+		owner: "abucin",
+		type: "bug",
+		description: "The authentication ignores whitespace."
+		}, {
+		code: "BG-5",
+		title: "Registration Page Header Missing",
+		status: "fixed",
+		owner: "abucin",
+		type: "bug",
+		description: "The registration page does not contain any headers."
+		}, {
+		code: "BG-8",
+		title: "Minor CSS Alignment Bug",
+		status: "in_progress",
+		owner: "abucin",
+		type: "bug",
+		description: "The logo is misaligned in IE6."
+		}, {
+		code: "BG-221",
+		title: "Fix CSS Button Padding",
+		status: "in_progress",
+		owner: "psmith",
+		type: "bug",
+		description: "The login button has extra padding."
+		}, {
+		code: "TA-21",
+		title: "Implement User Registration Mechanism",
+		status: "testing",
+		owner: "abucin",
+		type: "task",
+		description: "Create a secure mechanism for registering an account."
+		}, {
+		code: "TA-15",
+		title: "Review Currency Conversion Code",
+		status: "created",
+		owner: "abucin",
+		type: "task",
+		description: "See title."
+		}, {
+		code: "TA-4",
+		title: "Plan Review Meeting",
+		status: "in_progress",
+		owner: "abucin",
+		type: "task",
+		description: "This Thursday at 10:00."
+		}, {
+		code: "TA-8",
+		title: "Implement User Password Reset Functionality",
+		status: "created",
+		owner: "abucin",
+		type: "task",
+		description: "Create a secure mechanism for resetting a password for an account."
+		}, {
+		code: "TA-10",
+		title: "Revoke Domain Credentials",
+		status: "done",
+		owner: "abucin",
+		type: "task",
+		description: "Revoke domain credentials for inactive users."
+		}, {
+		code: "TA-110",
+		title: "Add Colour Palette",
+		status: "done",
+		owner: "psmith",
+		type: "task",
+		description: "Create a colour palette for the website."
+		}, {
+		code: "TA-130",
+		title: "Remove Redundant Tests",
+		status: "in_progress",
+		owner: "mlawrence",
+		type: "task",
+		description: "Remove tests that are not used."
+		}, {
+		code: "BG-130",
+		title: "Syntax Highlighting Broken",
+		status: "done",
+		owner: "mlawrence",
+		type: "bug",
+		description: "See title."
+		}
+	]);
 
-	tick.save(function (err, tick) {
-		if (err) return console.error(err);
-	});
+db.once('open', function callback() {
 
 	Ticket.find(function (err, tickets) {
 		if (err) return console.error(err);
-		console.log(tickets);
+		persistedTickets = tickets;
 	});
 
 });
@@ -40,103 +144,6 @@ db.once('open', function callback() {
 /**
  * Temp file which stores server data until a DB is created.
  */
-var mainData = {
-	tickets: [
-		{
-			code: "BG-15",
-			title: "Email Validation Not Working",
-			status: "testing",
-			owner: "abucin",
-			type: "bug",
-			description: "The email validation is broken for several users."
-		}, {
-			code: "BG-21",
-			title: "Authentication Whitespace Handling",
-			status: "created",
-			owner: "abucin",
-			type: "bug",
-			description: "The authentication ignores whitespace."
-		}, {
-			code: "BG-5",
-			title: "Registration Page Header Missing",
-			status: "fixed",
-			owner: "abucin",
-			type: "bug",
-			description: "The registration page does not contain any headers."
-		}, {
-			code: "BG-8",
-			title: "Minor CSS Alignment Bug",
-			status: "in_progress",
-			owner: "abucin",
-			type: "bug",
-			description: "The logo is misaligned in IE6."
-		}, {
-			code: "BG-221",
-			title: "Fix CSS Button Padding",
-			status: "in_progress",
-			owner: "psmith",
-			type: "bug",
-			description: "The login button has extra padding."
-		}, {
-			code: "TA-21",
-			title: "Implement User Registration Mechanism",
-			status: "testing",
-			owner: "abucin",
-			type: "task",
-			description: "Create a secure mechanism for registering an account."
-		}, {
-			code: "TA-15",
-			title: "Review Currency Conversion Code",
-			status: "created",
-			owner: "abucin",
-			type: "task",
-			description: "See title."
-		}, {
-			code: "TA-4",
-			title: "Plan Review Meeting",
-			status: "in_progress",
-			owner: "abucin",
-			type: "task",
-			description: "This Thursday at 10:00."
-		}, {
-			code: "TA-8",
-			title: "Implement User Password Reset Functionality",
-			status: "created",
-			owner: "abucin",
-			type: "task",
-			description: "Create a secure mechanism for resetting a password for an account."
-		}, {
-			code: "TA-10",
-			title: "Revoke Domain Credentials",
-			status: "done",
-			owner: "abucin",
-			type: "task",
-			description: "Revoke domain credentials for inactive users."
-		}, {
-			code: "TA-110",
-			title: "Add Colour Palette",
-			status: "done",
-			owner: "psmith",
-			type: "task",
-			description: "Create a colour palette for the website."
-		}, {
-			code: "TA-130",
-			title: "Remove Redundant Tests",
-			status: "in_progress",
-			owner: "mlawrence",
-			type: "task",
-			description: "Remove tests that are not used."
-		}, {
-			code: "BG-130",
-			title: "Syntax Highlighting Broken",
-			status: "done",
-			owner: "mlawrence",
-			type: "bug",
-			description: "See title."
-		}
-	]
-};
-
 var logData = {
 	"entries": [
 		{
@@ -218,7 +225,9 @@ var userData = {
 };
 
 exports.getMainData = function () {
-	return mainData;
+	return {
+		tickets: persistedTickets
+	};
 };
 
 exports.getLogData = function () {
