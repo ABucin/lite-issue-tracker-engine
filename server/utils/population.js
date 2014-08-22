0723632f27de189044138b5552c3a271ba698de8
Andrei Bucin <abucin@gmail.com>
2014-08-23 01:41:14 +0300

    Optimized data population script.

--- server/utils/population.js
+++ server/utils/population.js
@@ -1,3 +1,10 @@
+/**
+ * This script populates the issue tracker database with some dummy data so that its functionality can be tested.
+ */
+
+/**
+ * Dependent modules.
+ */
 var mongoose = require('mongoose'),
 	utils = require('./utils'),
 	Comment = require('../model/comment'),
@@ -6,10 +13,13 @@ var mongoose = require('mongoose'),
 	User = require('../model/user'),
 	Settings = require('../model/settings');
 
+/**
+ * Main DB population method.
+ */
 exports.populateDb = function () {
 
 	/**
-	 * Flushes all collections and adds some default data.
+	 * Flushes all collections before adding the default data.
 	 */
 	Settings.collection.drop();
 	Log.collection.drop();
@@ -17,10 +27,33 @@ exports.populateDb = function () {
 	Comment.collection.drop();
 	User.collection.drop();
 
-	var code = 0;
-	var userTickets = [];
-	var user2Tickets = [];
+	/**
+	 * Variable initializations.
+	 */
+	var code = 0,
+		userTickets = [],
+		user2Tickets = [],
+		userLogs = [],
+		user2Logs = [],
+		userComments = [],
+		user2Comments = [],
+		usernames = ["abucin", "psmith"],
+		priorities = ["minor", "normal", "major"],
+		globalSettings = [{
+			key: utils.generateKey(),
+			displayUserActivity: true,
+			displayUserChart: true,
+			displayUserEmail: true,
+			displayUserRole: true
+		}],
+		sampleTicketKey = utils.generateKey(),
+		yesterday = new Date();
 
+	yesterday.setDate(yesterday.getDate() - 1);
+
+	/**
+	 * Generates a ticket.
+	 */
 	var generateTicket = function (tickets, title, status, type, description, owner, priority, estimatedTime, loggedTime) {
 		return tickets.push({
 			key: utils.generateKey(),
@@ -36,23 +69,64 @@ exports.populateDb = function () {
 		});
 	}
 
-	generateTicket(userTickets, "Plan Review Meeting", "inProgress", "task", "This Thursday at 10:00.", "abucin", "normal", 10, 20);
-	generateTicket(userTickets, "Add Colour Palette", "testing", "task", "Create a colour palette for the website.", "", "major", 12, 12);
-	generateTicket(userTickets, "Remove Redundant Tests", "done", "task", "Remove tests that are not used.", "abucin", "minor", 10, 5);
-	generateTicket(userTickets, "Fix CSS Button Padding", "done", "bug", "The login button has extra padding.", "abucin", "minor", 10, 20);
-	generateTicket(userTickets, "Fix Responsive Menu", "inProgress", "bug", "The menu needs to be made responsive.", "abucin", "minor", 15, 10);
+	/**
+	 * Generates a ticket comment.
+	 */
+	var generateComment = function (comments, content) {
+		return comments.push({
+			key: utils.generateKey(),
+			content: content,
+			ticket: sampleTicketKey
+		});
+	}
 
-	generateTicket(user2Tickets, "Test Batch Script", "testing", "task", "The deployment script should be tested.", "psmith", "normal", 25, 13);
-	generateTicket(user2Tickets, "Fix Missing Authentication Header.", "inProgress", "bug", "Add the missing header.", "psmith", "major", 5, 26);
-	generateTicket(user2Tickets, "Fix Login Page CSS", "done", "bug", "Fix Login Page CSS.", "psmith", "major", 15, 23);
+	/**
+	 * Generates a log entry.
+	 */
+	var generateLog = function (logs, action, amount, target, targetType, comment, username, timestamp) {
+		return logs.push({
+			key: utils.generateKey(),
+			action: action,
+			amount: amount != null ? amount : 0,
+			target: target,
+			targetType: targetType,
+			comment: comment != null ? comment : "",
+			username: username,
+			timestamp: timestamp != null ? timestamp : new Date()
+		});
+	}
 
-	var sampleTicketKey = utils.generateKey();
-	var yesterday = new Date();
-	yesterday.setDate(yesterday.getDate() - 1);
+	/**
+	 * Dummy-data generation.
+	 */
+	generateTicket(userTickets, "Plan Review Meeting", "inProgress", "task", "This Thursday at 10:00.", usernames[0], priorities[1], 10, 20);
+	generateTicket(userTickets, "Add Colour Palette", "testing", "task", "Create a colour palette for the website.", "", priorities[2], 12, 12);
+	generateTicket(userTickets, "Remove Redundant Tests", "done", "task", "Remove tests that are not used.", usernames[0], priorities[0], 10, 5);
+	generateTicket(userTickets, "Fix CSS Button Padding", "done", "bug", "The login button has extra padding.", usernames[0], priorities[0], 10, 20);
+	generateTicket(userTickets, "Fix Responsive Menu", "inProgress", "bug", "The menu needs to be made responsive.", usernames[0], priorities[0], 15, 10);
 
+	generateTicket(user2Tickets, "Test Batch Script", "testing", "task", "The deployment script should be tested.", usernames[1], priorities[1], 25, 13);
+	generateTicket(user2Tickets, "Fix Missing Authentication Header.", "inProgress", "bug", "Add the missing header.", usernames[1], priorities[2], 5, 26);
+	generateTicket(user2Tickets, "Fix Login Page CSS", "done", "bug", "Fix Login Page CSS.", usernames[1], priorities[2], 15, 23);
+
+	generateLog(userLogs, "comment", null, "1", "task", "I think this feature should be implemented in the next sprint.", usernames[0], null);
+	generateLog(userLogs, "clock-o", 3, "2", "task", null, usernames[0], yesterday);
+	generateLog(userLogs, "comment", null, "3", "bug", "Bug fixed in commit 3d6h4.", usernames[0], null);
+
+	generateLog(user2Logs, "clock-o", 4, "6", "task", null, usernames[1], null);
+	generateLog(user2Logs, "clock-o", 6, "6", "task", null, usernames[1], yesterday);
+
+	generateComment(userComments, "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.");
+	generateComment(userComments, "That is correct. I will do it soon.");
+
+	generateComment(user2Comments, "Also, please update the title to something more accurate.");
+
+	/**
+	 * User generation.
+	 */
 	var user = new User({
 		key: 123,
-		username: "abucin",
+		username: usernames[0],
 		email: "abucin@gmail.com",
 		firstName: "Andrei",
 		lastName: "Bucin",
@@ -63,52 +137,14 @@ exports.populateDb = function () {
 		project: "issue-tracker",
 		expertise: "Java, PHP, JavaScript, Web Design.",
 		tickets: userTickets,
-		settings: [{
-			key: utils.generateKey(),
-			displayUserActivity: true,
-			displayUserChart: true,
-			displayUserEmail: true,
-			displayUserRole: true
-		}],
-		logs: [{
-			key: utils.generateKey(),
-			action: "comment",
-			target: "1",
-			targetType: "task",
-			comment: "I think this feature should be implemented in the next sprint.",
-			username: "abucin",
-			timestamp: new Date()
-		}, {
-			key: utils.generateKey(),
-			action: "clock-o",
-			amount: 3,
-			target: "2",
-			targetType: "task",
-			username: "abucin",
-			timestamp: yesterday
-		}, {
-			key: utils.generateKey(),
-			action: "comment",
-			target: "3",
-			targetType: "bug",
-			comment: "Bug fixed in commit 3d6h4.",
-			username: "abucin",
-			timestamp: new Date()
-		}],
-		comments: [{
-			key: utils.generateKey(),
-			content: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.",
-			ticket: sampleTicketKey
-		}, {
-			key: utils.generateKey(),
-			content: "That is correct. I will do it soon.",
-			ticket: sampleTicketKey
-		}]
+		settings: globalSettings,
+		logs: userLogs,
+		comments: userComments
 	});
 
 	var user2 = new User({
 		key: 124,
-		username: "psmith",
+		username: usernames[1],
 		email: "psmith@gmail.com",
 		firstName: "Peter",
 		lastName: "Smith",
@@ -119,43 +155,20 @@ exports.populateDb = function () {
 		project: "unassigned",
 		expertise: "JavaScript, HTML5, CSS3.",
 		tickets: user2Tickets,
-		settings: [{
-			key: utils.generateKey(),
-			displayUserActivity: true,
-			displayUserChart: true,
-			displayUserEmail: true,
-			displayUserRole: true
-		}],
-		logs: [{
-			key: utils.generateKey(),
-			action: "clock-o",
-			amount: 4,
-			target: "6",
-			targetType: "task",
-			username: "psmith",
-			timestamp: new Date()
-		}, {
-			key: utils.generateKey(),
-			action: "clock-o",
-			amount: 6,
-			target: "6",
-			targetType: "task",
-			username: "psmith",
-			timestamp: yesterday
-		}],
-		comments: [{
-			key: utils.generateKey(),
-			content: "Also, please update the title to something more accurate.",
-			ticket: sampleTicketKey
-		}]
+		settings: globalSettings,
+		logs: user2Logs,
+		comments: user2Comments
 	});
 
-	user.setPassword("abucin", function () {
+	/**
+	 * Password generation.
+	 */
+	user.setPassword(usernames[0], function () {
 		user.save(function (err) {
 			if (err) {
 				return console.error(err); // we should handle this
 			} else {
-				user2.setPassword("psmith", function () {
+				user2.setPassword(usernames[1], function () {
 					user2.save(function (err) {
 						if (err) {
 							return console.error(err); // we should handle this
